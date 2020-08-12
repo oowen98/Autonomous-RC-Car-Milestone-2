8741cde128c40789563c2d5d397e058f80eaaf09
dlinano <owen.ren077@gmail.com>
2020-08-11 21:29:32 -0700

    Ran Model on Jetson Nano, needs more improvements

--- main.py
+++ main.py
@@ -16,12 +16,11 @@ import time
 from PIL import Image
 import torch
 
-from PyTorch_NeuralNetwork import CNN1
-
+import PyTorch_NeuralNetwork
 from datetime import datetime
 
 def frame_preprocessing(frame):
-    '''
+    
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #Converting from opencv image to PIL image
     img_pil = Image.fromarray(img)
 
@@ -33,6 +32,7 @@ def frame_preprocessing(frame):
     frame_color = color(frame_resize)
     img_tensor = to_tensor(frame_color)
     img_tensor = img_tensor.unsqueeze(0)
+    #img_tensor = img_tensor.to(torch.device('cuda'))
     '''
     device = torch.device('cuda')
 
@@ -42,7 +42,8 @@ def frame_preprocessing(frame):
     frame = torch.from_numpy(frame).float()
     frame = frame.to(device)
     frame = frame[None, ...]
-    return frame
+    '''
+    return img_tensor
 
 def map_value(value, lower_in=0, upper_in=180, lower_out=-100, upper_out=100):
     return ((value - lower_in) * (upper_out - lower_out) // (upper_in - lower_in) + lower_out)
@@ -84,14 +85,14 @@ if __name__ == '__main__':
     frame_cnt = 60
     i = 0
     start_time = (time.time()-1)
-    model = Net2()
+    model = PyTorch_NeuralNetwork.Net2()
     model.eval()
     print("Loading Neural Network")
     model.load_state_dict(torch.load('/home/dlinano/Desktop/Autonomous-RC-Car-Milestone-2/Data_Collection/PyTorch_Training/Autonomous_RC_Car_Net2_3.pt')) #Load Model
-    model.cuda()
+    #model.cuda()
     #model.eval()
-    #device = torch.device('cuda')
-    #model.to(device)
+    device = torch.device('cuda')
+    model.to(device)
     print("loaded Neural Network")
 
     data_transform = transforms.Compose([transforms.Resize((224,224)), transforms.ToTensor()])
@@ -102,13 +103,15 @@ if __name__ == '__main__':
         frame = camera.read()
 
         img_tensor = frame_preprocessing(frame)
+        img_tensor = img_tensor.to(device)
         #img_pil = Image.fromarray(frame)
         #img_tensor = data_transform(img_pil).unsqueeze(0).cuda()
         
         
         
-        
-        output = model(img_tensor)
+        with torch.no_grad():
+            output = model(img_tensor)
+       
         print("ouput: ",output)
         steering = int(180*output[0,1])
         throttle = int(180*output[0,0])
@@ -117,8 +120,8 @@ if __name__ == '__main__':
 
         #cv2.imshow('frame', frame)
 
-        if (throttle > 125):
-            throttle = 125
+        if (throttle > 113):
+            throttle = 113
         if (steering > 255):
             steering = 90
         print("Throttle: ", throttle, " Steering: ", steering)
